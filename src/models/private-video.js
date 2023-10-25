7813e9f89c40e6a2531d2d4e68e711c4c1767ed8
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-10-25 21:34:17 +0100

    delete user and get private videos

--- src/models/private-video.js
+++ src/models/private-video.js
@@ -1,44 +1,41 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const privateVideoSchema = new mongoose.Schema({
-    // Reference to the Comment model (foreign key)
-    comment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'PrivateComment',
-    },
-    user: {
-      type: String,
-      ref: 'user',
-      required: true,
-    },
-    videoId: {
-      type: String,
-      required: true,
-    },
-    videoTitle: {
-      type: String,
-    },
-    videoURL:{
-      type: String,
-    },
-    description: {
-      type: String,
-    },
-    likes:{
-      type: Map
-    },
-    views: {
-        type: String,
-      },
-    uploadedAt: {
-      type: Date,
-      default: Date.now,
-    },
-    tags: {
-      type: [String], // Array of strings
-      
+  // Reference to the Comment model (foreign key)
+  comment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PrivateComment",
   },
-  });
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  videoId: {
+    type: String,
+    required: true,
+  },
+  videoTitle: {
+    type: String,
+  },
+  videoURL: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  likes: {
+    type: Map,
+  },
+  views: {
+    type: String,
+  },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  tags: {
+    type: [String], // Array of strings
+  },
+});
 
-  module.exports = mongoose.model('PrivateVideo', privateVideoSchema);
+module.exports = mongoose.model("PrivateVideo", privateVideoSchema);
--- src/services/videoServices.js
+++ src/services/videoServices.js
@@ -1,52 +1,52 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const createReadStream = fs.createReadStream;
-const OpenAI = require('openai');
-const PublicVideo = require('../models/public-video')
-const cloudinary =require('cloudinary').v2; 
-const { CloudinaryStorage } =require('multer-storage-cloudinary');
-const PublicComment = require('../models/privateComment');
+const OpenAI = require("openai");
+const PublicVideo = require("../models/public-video");
+const PrivateVideo = require("../models/private-video");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const PublicComment = require("../models/privateComment");
 //const { io } = require('../../index');
 
-
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 const cloudinaryStorage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'samples', 
-    allowed_formats: ['mp4', 'avi', 'mkv', 'jpeg'], 
+    folder: "samples",
+    allowed_formats: ["mp4", "avi", "mkv", "jpeg"],
   },
 });
 
 const OpenAIAPIKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({
-  apiKey: OpenAIAPIKey
+  apiKey: OpenAIAPIKey,
 });
 
-
-async function uploadVideo( videoBuffer ) {
+async function uploadVideo(videoBuffer) {
   try {
-    const {secure_url}  = await cloudinary.uploader.upload(`data:video/mp4;base64,${videoBuffer.toString('base64')}`, {
-      resource_type: 'video',
-    });
+    const { secure_url } = await cloudinary.uploader.upload(
+      `data:video/mp4;base64,${videoBuffer.toString("base64")}`,
+      {
+        resource_type: "video",
+      }
+    );
 
     return secure_url;
-
   } catch (error) {
-    throw new Error('Error uploading video to Cloudinary: ' + error.message );
+    throw new Error("Error uploading video to Cloudinary: " + error.message);
   }
 }
 
-async function transcribeAudio (audioBuffer){
+async function transcribeAudio(audioBuffer) {
   try {
-    const tempFilePath = 'audio.m4a';
+    const tempFilePath = "audio.m4a";
 
     await writeFileAsync(tempFilePath, audioBuffer);
 
@@ -60,39 +60,38 @@ async function transcribeAudio (audioBuffer){
     fs.unlinkSync(tempFilePath);
     return transcriptionResult.text;
   } catch (error) {
-    console.error('Error transcribing audio:', error);
+    console.error("Error transcribing audio:", error);
     throw error;
   }
 }
 
 async function saveVideoAndTranscription(videoBuffer, audioBuffer, reqBody) {
   try {
-      const audioTranscription = await transcribeAudio(audioBuffer);
-      const videoUrl = await uploadVideo(videoBuffer);
+    const audioTranscription = await transcribeAudio(audioBuffer);
+    const videoUrl = await uploadVideo(videoBuffer);
 
-      const videoData = {
-          videoTitle: reqBody.title,
-          videoSummary: reqBody.summary,
-          tags: reqBody.tags,
-          videoURL: videoUrl,
-          transcription: audioTranscription
-      };
+    const videoData = {
+      videoTitle: reqBody.title,
+      videoSummary: reqBody.summary,
+      tags: reqBody.tags,
+      videoURL: videoUrl,
+      transcription: audioTranscription,
+    };
 
-      const VideoModel = reqBody.isPublic ? PublicVideo : PrivateVideo;
+    const VideoModel = reqBody.isPublic ? PublicVideo : PrivateVideo;
 
-      const video = new VideoModel(videoData);
-      await video.save();
+    const video = new VideoModel(videoData);
+    await video.save();
 
-      return {
-          message: "Video upload and transcription successful",
-          videoObj: videoData
-      };
+    return {
+      message: "Video upload and transcription successful",
+      videoObj: videoData,
+    };
   } catch (error) {
-      console.error('Error:', error);
-      throw error;
+    console.error("Error:", error);
+    throw error;
   }
 }
-
 
 async function getVideoMetadata(publicUrl) {
   try {
@@ -101,7 +100,9 @@ async function getVideoMetadata(publicUrl) {
     // Return video metadata
     return video;
   } catch (error) {
-    throw new Error('Error retrieving video metadata from Cloudinary: ' + error.message);
+    throw new Error(
+      "Error retrieving video metadata from Cloudinary: " + error.message
+    );
   }
 }
 
@@ -110,47 +111,88 @@ async function deleteVideo(publicId) {
     const result = await cloudinary.uploader.destroy(publicId);
 
     // Return deletion result
-    return result.result === 'ok';
+    return result.result === "ok";
   } catch (error) {
-    throw new Error('Error deleting video from Cloudinary: ' + error.message);
+    throw new Error("Error deleting video from Cloudinary: " + error.message);
   }
 }
 
-async function fetchVideoById(videoId){
+async function fetchVideoById(videoId) {
   try {
     const video = await PublicVideo.findOne({ videoId });
 
     return video;
   } catch (error) {
-    throw new Error('Error fetching video by videoId');
+    throw new Error("Error fetching video by videoId");
   }
 }
 
-async function fetchAllPublicVideos(pageNumber){
+async function fetchAllPublicVideos(pageNumber) {
   try {
-      const page = parseInt(pageNumber, 10) || 5;
-      const pageSize = parseInt("10", 10) || 20;
+    const page = parseInt(pageNumber, 10) || 5;
+    const pageSize = parseInt("10", 10) || 20;
 
-      const videoList = await PublicVideo.aggregate([
-        {
-          $facet: {
-            metadata: [{ $count: 'totalCount' }],
-            data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
-          },
+    const videoList = await PublicVideo.aggregate([
+      {
+        $facet: {
+          metadata: [{ $count: "totalCount" }],
+          data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
         },
-      ]);
+      },
+    ]);
 
-      const response = {
-        success: true,
-        videos: {
-          metadata: { totalCount: videoList[0].metadata[0].totalCount, page, pageSize },
-          data: videoList[0].data,
+    const response = {
+      success: true,
+      videos: {
+        metadata: {
+          totalCount: videoList[0].metadata[0].totalCount,
+          page,
+          pageSize,
         },
-      };
+        data: videoList[0].data,
+      },
+    };
 
     return response;
   } catch (error) {
-    throw new Error('Error retrieving videos');
+    throw new Error("Error retrieving videos");
+  }
+}
+
+async function fetchAllPrivateVideos(pageNumber, userId) {
+  try {
+    const page = parseInt(pageNumber, 10) || 1;
+    const pageSize = parseInt("10", 10) || 6;
+
+    const videoList = await PrivateVideo.aggregate([
+      {
+        $match: {
+          user: mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $facet: {
+          metadata: [{ $count: "totalCount" }],
+          data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
+        },
+      },
+    ]);
+
+    const response = {
+      success: true,
+      videos: {
+        metadata: {
+          totalCount: videoList[0].metadata[0].totalCount,
+          page,
+          pageSize,
+        },
+        data: videoList[0].data,
+      },
+    };
+
+    return response;
+  } catch (error) {
+    throw new Error("Error retrieving videos");
   }
 }
 
@@ -180,13 +222,13 @@ async function searchVideosByDate(query) {
 async function searchVideosByTitle(query) {
   try {
     // Create a regex pattern for the title query to perform a case-insensitive search
-    const titlePattern = new RegExp(query, 'i');
+    const titlePattern = new RegExp(query, "i");
 
     // Search for videos with titles matching the title query
     const videos = await PublicVideo.find({
       videoTitle: titlePattern, // Title matching the title query
     });
-    
+
     return videos;
   } catch (error) {
     console.error("Error searching videos by title:", error);
@@ -199,14 +241,13 @@ async function increaseViewCount(videoId) {
     // Find the video by its ID and update the view count
     const video = await PublicVideo.findById(videoId);
     if (!video) {
-      throw new Error('Video not found');
+      throw new Error("Video not found");
     }
 
     video.views += 1;
     await video.save();
-
   } catch (error) {
-    console.error('Error increasing view count:', error);
+    console.error("Error increasing view count:", error);
     throw error;
   }
 }
@@ -220,5 +261,6 @@ module.exports = {
   searchVideosByDate,
   increaseViewCount,
   searchVideosByTitle,
-  cloudinaryStorage
+  cloudinaryStorage,
+  fetchAllPrivateVideos,
 };
